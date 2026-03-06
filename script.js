03e83b30ac8eae2660ccd8e140c7995fcc3c8f72
juhairulislam <juhairulislam2018@gmail.com>
2026-03-06 09:52:20 +0600

    loading spinner , click category button to fetch trees by category ID by API

--- script.js
+++ script.js
@@ -3,7 +3,23 @@
 
 const categoriesContainer = document.getElementById('categoriesContainer')
 const treesContainer = document.getElementById('trees-container');
+const loadingSpinner = document.getElementById('loading-spinner') ;
 
+
+// function for loading spinner
+function showLoading(status){
+
+    if(status === true ){
+        loadingSpinner.classList.remove('hidden') ;
+        treesContainer.classList.add('hidden') ;
+    } else{
+
+        treesContainer.classList.remove('hidden') ;
+        loadingSpinner.classList.add('hidden') ;
+
+    }
+
+}
 // function for categories
 
 async function loadCategories() {
@@ -19,17 +35,54 @@ async function loadCategories() {
         const btn = document.createElement('button');
         btn.className = 'btn btn-outline w-full';
         btn.textContent = element.category_name;
+        btn.onclick = () => selectCategory(element.id , btn)
         categoriesContainer.appendChild(btn)
     })
 }
+
+
+// function for category onclick 
+async function selectCategory(categoryId , btn){
+    console.log(categoryId , btn)
+
+    showLoading(true)  ;
+
+    const allButton = document.querySelectorAll('#categoriesContainer button , #allTrees') ;
+    
+    // step 1 -- every button make btn outline
+    allButton.forEach(btn => {
+        btn.classList.remove('btn-primary') ;
+        btn.classList.add('btn-outline')
+        
+    })
+
+    // paramiter theke asha btn take add btn-primary
+    btn.classList.add('btn-primary') ;  
+    btn.classList.remove('btn-outline') ;
+
+
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`) ;
+
+    const data = await res.json() ;
+    console.log(data)
+    showLoading(false)
+    displayTrees(data.plants) ;
+}
+
+
+
 
 
 // function for trees 
 
 async function loadTrees() {
 
+    showLoading(true) ;
+
     const res = await fetch('https://openapi.programming-hero.com/api/plants');
     const data = await res.json();
+    showLoading(false) ;
     displayTrees(data.plants);
 
 }
@@ -38,9 +91,10 @@ async function loadTrees() {
 function displayTrees(trees) {
 
     // console.log(trees)
+    treesContainer.innerHTML = '' ;
 
     trees.forEach(tree => {
-        console.log(tree)
+        // console.log(tree)
         const card = document.createElement('div');
         card.className = "card bg-white w-[95%]  shadow-sm "
         card.innerHTML = `
@@ -55,7 +109,7 @@ function displayTrees(trees) {
                         <div class="badge  badge-outline badge-success bg-green-100">${tree.category}</div>
 
                         <div class="flex justify-between items-center">
-                            <p class="font-bold text-xl text-[#15803D] text-left">$5000</p>
+                            <p class="font-bold text-xl text-[#15803D] text-left">$${tree.price}</p>
                             <button class="btn btn-primary bg-[#15803D]">Cart</button>
                         </div>
                     </div>
@@ -63,7 +117,8 @@ function displayTrees(trees) {
         `
 
 
-        treesContainer.appendChild(card)
+        treesContainer.appendChild(card) ;
+        
     })
 
 }
